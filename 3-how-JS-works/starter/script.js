4c0d64e53508a1d2b8d82fa00c024366c0324aae
JoeyJr37 <JoeyJr11@gmail.com>
2019-10-27 07:27:11 -0500

    added code from video tutorial

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,7 +1,17 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+calculateAge(1965);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+};
+
+var retirement = function(year){
+    console.log(65-(2016-year));
+}
+
+retirement(1990);
 
 
 

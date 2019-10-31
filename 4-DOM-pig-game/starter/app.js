0b3c0cfa80aec30b6123398770a96c187393ccf3
JoeyJr37 <JoeyJr11@gmail.com>
2019-10-31 06:12:42 -0500

    added Event listeners for dice game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable quotes */
+/* eslint-disable prefer-arrow-callback */
 /*
 GAME RULES:
 
@@ -10,12 +12,41 @@ After that, it's the next player's turn
 */
 let scores = [0,0];
 let roundScore = 0;
-let activePlayer = 1;
-let dice = Math.floor(Math.random() * 6) + 1;
-document.querySelector("#current-" + activePlayer).textContent = dice;
-//document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
+let activePlayer = 0;
 
-let x = document.querySelector("#score-0").textContent;
-console.log(x);
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
+// let x = document.querySelector("#score-0").textContent;
+// console.log(x);
 
-document.querySelector(".dice").style.display = "none";
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+  // 1. random number generated
+  let dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. display the result
+  let diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer =0;
+    roundScore = 0;
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDOM.style.display = "none";
+  }
+});
